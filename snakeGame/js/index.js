2e84db6f6593ffd0ed12eb05fa9dc59344c4876a
Priyanshu <pshekhar830@gmail.com>
2023-03-13 11:59:01 +0530

    added sound for eating food

--- snakeGame/js/index.js
+++ snakeGame/js/index.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 2;
+let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -42,11 +42,21 @@ function gameEngine(){
 
     // If the food is eaten, increment the score and regenerate the food
     if(snakeArr[0].x === food.x && snakeArr[0].y === food.y){
+        foodSound.play();
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
         let a = 2;
         let b = 16;
         food = {x: Math.round(a + (b-a)*Math.random()), y: Math.round(a + (b-a)*Math.random())};
     }
+
+    // Moving the snake
+    for(let i=snakeArr.length-2; i>=0; i--){
+        // const element = array[i]
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y; 
 
 
     // Part2: Display the snake and food
